b62a1b999d1bc9f238bc7f17b19e1770ad1bf1ba
K_Vebere <vebere2204@gmail.com>
2025-08-17 17:31:04 +0100

    yeet

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UI From Hell</title>
+
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="contact.html">Home</a>
+        <a href="about.html">Home</a>
+    </nav>
+    <main>
+        <h1 class="heading" id="mainhead">Welcome to my UI From Hell!</h1>
+        <p>We will provide you with:</p>
+        <ul>
+            <li>Pain</li>
+            <li>Suffering</li>
+            <li>Headache</li>
+        </ul>
+        <h2>Click here to proceed!</h2>
+    </main>
+</body>
+</html>
